a56f35c3a2e7f7b139b3fbe234b0c49cf941db31
JAreina <jareinafdez@gmail.com>
2018-03-03 09:27:51 +0100

    enums, arrays

--- 1-ts/salida/tipos.js
+++ 1-ts/salida/tipos.js
@@ -3,3 +3,26 @@ var numero;
 numero = 11111;
 console.log(numero);
 var booleano = false;
+// arrays
+var arr = [1, 2, 3];
+console.log("array", arr);
+var arrr = ['a', 'b'];
+console.log("array " + arrr);
+//enums
+var dia;
+(function (dia) {
+    dia[dia["MARTES"] = 0] = "MARTES";
+    dia[dia["MIERCOLES"] = 1] = "MIERCOLES";
+})(dia || (dia = {}));
+;
+var dia2;
+(function (dia2) {
+    dia2[dia2["JUEVES"] = 0] = "JUEVES";
+    dia2[dia2["VIERNES"] = 1] = "VIERNES";
+})(dia2 || (dia2 = {}));
+;
+var primerDia;
+primerDia = dia.MARTES;
+console.log("enum: " + primerDia);
+console.log("enum VALOR:  " + dia[0]);
+console.log("enum VALOR: " + dia[dia.MARTES]);
--- 1-ts/src/tipos/tipos.ts
+++ 1-ts/src/tipos/tipos.ts
@@ -5,3 +5,28 @@ numero =11111;
 console.log(numero)
 
 let booleano :boolean=false;
+
+
+// arrays
+
+let arr:number[]=[1,2,3];
+console.log("array" ,arr);
+
+
+let arrr:Array<string>=['a','b'];
+
+console.log("array "+arrr);
+
+
+//enums
+
+enum dia {MARTES,MIERCOLES};
+enum dia2 {JUEVES,VIERNES};
+
+let primerDia : dia;
+primerDia = dia.MARTES;
+
+console.log("enum: "+primerDia);
+
+console.log("enum VALOR:  "+dia[0]);
+console.log("enum VALOR: "+dia[dia.MARTES]);
