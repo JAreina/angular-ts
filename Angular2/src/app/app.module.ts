736bb49ea31594e531762dc2eb6bd48cc94d9313
JAreina <jareinafdez@gmail.com>
2018-03-04 20:11:38 +0100

    angular material, theme, animations en app module

--- Angular2/src/app/app.module.ts
+++ Angular2/src/app/app.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RutasModule } from './rutas/rutas.module';
-
+import { LoginComponent } from './componentes/login/login.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [ //nuestros compoentes
-    AppComponent
+    AppComponent, LoginComponent
   ],
   imports: [// dependencias del modulo
-    BrowserModule,RutasModule
+    BrowserModule,RutasModule,
+    BrowserAnimationsModule  // para animaciones 
   ],
   providers: [],
   bootstrap: [AppComponent]
