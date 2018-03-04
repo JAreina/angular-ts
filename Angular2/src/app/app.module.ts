ceac409e6d1f8a17a843af6e20b5be57266368ed
JAreina <jareinafdez@gmail.com>
2018-03-04 10:40:45 +0100

    rutas

--- Angular2/src/app/app.module.ts
+++ Angular2/src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RutasModule } from './rutas/rutas.module';
 
 @NgModule({
-  declarations: [
+  declarations: [ //nuestros compoentes
     AppComponent
   ],
-  imports: [
-    BrowserModule
+  imports: [// dependencias del modulo
+    BrowserModule,RutasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
