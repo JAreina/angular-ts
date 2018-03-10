949005ba2a4464e5e4e773fd3ff326d800d7d497
JAreina <jareinafdez@gmail.com>
2018-03-10 10:35:33 +0100

    menu

--- Angular2/src/app/rutas/rutas-routing.module.ts
+++ Angular2/src/app/rutas/rutas-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UnoComponent } from '../componentes/material/uno/uno.component';
+import { AppComponent } from '../app.component';
 
 const routes: Routes = [
+ 
   {path:'uno',
   component: UnoComponent
 }
