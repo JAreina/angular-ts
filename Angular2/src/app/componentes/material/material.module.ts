8544fc8548d747922677dfa87ce4c6f505f22a27
JAreina <jareinafdez@gmail.com>
2018-03-10 09:56:48 +0100

    componente toolbar material

--- Angular2/src/app/componentes/material/material.module.ts
+++ Angular2/src/app/componentes/material/material.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UnoComponent } from './uno/uno.component';
 
+//importo para crear el toolbar
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule
+  ],
+  exports: 
+  [ //export para poder usar en otros componentes
+    MatToolbarModule
   ],
   declarations: [UnoComponent]
 })
