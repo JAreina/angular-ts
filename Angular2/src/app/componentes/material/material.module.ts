72ffc2aac3d59eee366595b0fbecc07b8eea421b
JAreina <jareinafdez@gmail.com>
2018-03-10 10:20:17 +0100

    iconos

--- Angular2/src/app/componentes/material/material.module.ts
+++ Angular2/src/app/componentes/material/material.module.ts
@@ -4,16 +4,24 @@ import { UnoComponent } from './uno/uno.component';
 
 //importo para crear el toolbar
 import {MatToolbarModule} from '@angular/material/toolbar';
+//importar boton
+import {MatButtonModule} from '@angular/material/button';
+//importar iconos
+import {MatIconModule} from '@angular/material/icon';
 
 
 @NgModule({
   imports: [
     CommonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
   ],
   exports: 
   [ //export para poder usar en otros componentes
-    MatToolbarModule
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
   ],
   declarations: [UnoComponent]
 })
